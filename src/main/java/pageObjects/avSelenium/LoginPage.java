964c379e7a301fe356f8f12d92b319a46091956d
= <=>
2022-11-29 23:11:07 +0300

    Add jenkins file

--- src/main/java/pageObjects/avSelenium/LoginPage.java
+++ src/main/java/pageObjects/avSelenium/LoginPage.java
@@ -19,7 +19,7 @@ public class LoginPage extends BasePage {
 
     public LoginPage enterEmail() {
         log.debug("Enter email");
-        enter(this.email, properties.getProperty("email"));
+        enter(this.email, "qatest808080@gmail.com");
         return this;
     }
 
